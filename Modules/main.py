0c00bd993a66d97d05058a1678a863952e217aae
rockybai327 <rockybai327@gmail.com>
2026-04-10 11:18:01 +0530

    main.py

--- Modules/main.py
+++ Modules/main.py
@@ -39,9 +39,9 @@ bot = Client(
     bot_token=BOT_TOKEN
 )
 
-API_ID    = os.environ.get("API_ID", "22492036")
-API_HASH  = os.environ.get("API_HASH", "b4d672fb51ddf8abaa62c6db1f756babb")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6118360596:AAESJ47idHFsaTZcMvrBbSGeMrhP7XnX9YA") 
+API_ID    = os.environ.get("API_ID", "")
+API_HASH  = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
 # Define aiohttp routes
 routes = web.RouteTableDef()
